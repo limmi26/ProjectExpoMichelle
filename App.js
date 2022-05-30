61fb41aad5b637fada7ea0ea38b7fb157fa46973
limmi26 <kisamakiazuel@gmail.com>
2022-05-30 14:32:45 +0700

    perubahan 1

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      {/* Rest of your app code */}
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
+    
   );
 }
 
